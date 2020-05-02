77e548dae9ccf6cf770ada843530bb6ce0eb0056
mohammedgehad <pu2k@hotmail.com>
2020-05-02 20:55:58 +0530

    server-side hello world

---
+++ server-side/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
